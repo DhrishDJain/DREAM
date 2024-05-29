648a8aada439c342b60519ad03691443ea39625f
Dhrish Dinesh Jain <jainfenil12@gmail.com>
2024-05-29 20:56:12 +0530

    converted to node project and tailwind setuped

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.{html,js}"],
+  content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary':'#21a6ff'
+      }
+    },
+
   },
+  
   plugins: [],
 }
 
